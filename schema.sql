c8a9a4ed5ec5ac9c08c454b8f349a50d74b60de6
benjaminwirz07 <benjaminwirz10@gmail.com>
2026-09-03 19:36:34 -0300

    cambio de nombre base de datos

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE pdi_base;
+CREATE DATABASE Allticket
 
-USE pdi_base;
+USE Allticket;
 
 CREATE TABLE IF NOT EXISTS users (
     id         CHAR(36)     NOT NULL DEFAULT (UUID()),
--- src/controllers/auth/login.php
+++ src/controllers/auth/login.php
@@ -1,2 +1,14 @@
 <?php
-# Implementar...
+require_once __DIR__ . '/../../config/bootstrap.php';
+
+// Paso clave #1: Validar tipo de solicitud ----------------------
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {  // Si la solicitud no es POST, volves al register
+  header('Location: /src/views/auth/login.php');
+  exit;
+}
+
+// Paso clave #2: Tomar datos -----------------------------------
+$data = [
+  'email'           => trim($_POST['email'] ?? ''), // trim(str) saca los espacios al inicio y al final
+  'password'        => $_POST['password'] ?? '',
+];
