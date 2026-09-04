7c71460b3383401ac2edaf07ae3a03f46bc229dc
benjaminwirz07 <benjaminwirz10@gmail.com>
2026-09-03 21:23:52 -0300

    register-validaciones

--- src/controllers/auth/login.php
+++ src/controllers/auth/login.php
@@ -12,3 +12,19 @@ $data = [
   'email'           => trim($_POST['email'] ?? ''), // trim(str) saca los espacios al inicio y al final
   'password'        => $_POST['password'] ?? '',
 ];
+// Paso clave #3: Validar datos
+             
+//paso clave #4 validar q el usuario exista
+
+//paso clave #5 validar si el usuario existe y la contraseña es correcta
+
+//paso clave #6 logear al usuario 
+
+try {
+  // Lógica de validación y autenticación
+} catch (Exception $e) {
+  $_SESSION['error'] = $e->getMessage();
+  header('Location: /src/views/auth/login.php');
+  exit;
+}
+?>
